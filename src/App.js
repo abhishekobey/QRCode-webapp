c5c09bd3cd7eab9568970d863894c01747e53547
abhishekobey <70887064+abhishekobey@users.noreply.github.com>
2022-04-18 17:31:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>Scan QR Code</h2>
-          <QRCode value="hello" />
+          <QRCode value="https://drive.google.com/file/d/0B6wAA3DLjQiyc3RhcnRlcl9maWxl/view?usp=sharing&resourcekey=0-zeiYwOWuhWvJkyTt_RZr0Q" />
       </header>
     </div>
   );
